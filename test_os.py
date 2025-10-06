cc79da44999992bd9a98262fe51dae49e4a56a86
proger164 <gerasimenkosv@bk.ru>
2025-10-06 19:37:51 +0300

    Первичное создание проекта

---
+++ test_os.py
@@ -0,0 +1,16 @@
+import os
+
+os.makedirs("C:/1/demo", exist_ok=True)
+
+my_dir = "D:/"
+
+my_list = os.listdir(my_dir)
+
+# Задача - среди всех папок и файлов на диске Д найти только текстовые файлы
+
+txt_files = []
+
+for item in my_list:
+    if os.path.isfile(os.path.join(my_dir,item)) and item.endswith(".txt"):
+        txt_files.append(item)
+print(txt_files)
